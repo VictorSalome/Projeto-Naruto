ef2f8f774bf0ea0eee8dff5307fc8f90902ca7a3
Victor Salome <vsalome41@gmail.com>
2024-05-09 18:21:09 -0300

    feat: finalização da implementação do carousel

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,25 +1,9 @@
-import { ViewCarousel } from "@mui/icons-material";
+import { Carousel } from "./components/carousel";
 
 export const Home = () => {
-  const images = [
-    { src: "https://via.placeholder.com/500x300", alt: "Image 1" },
-    { src: "https://via.placeholder.com/500x300", alt: "Image 2" },
-    { src: "https://via.placeholder.com/500x300", alt: "Image 3" },
-    { src: "https://via.placeholder.com/500x300", alt: "Image 4" },
-  ];
   return (
-    <div>
-      {" "}
-      <ViewCarousel autoPlay interval={3000} animation="fade">
-        {images.map((image, index) => (
-          <img
-            key={index}
-            src={image.src}
-            alt={image.alt}
-            style={{ width: "100%", height: "auto" }}
-          />
-        ))}
-      </ViewCarousel>
-    </div>
+    <>
+      <Carousel />
+    </>
   );
 };
