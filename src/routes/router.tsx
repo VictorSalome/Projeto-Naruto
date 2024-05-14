941383d38c55e07eac85e988d9296aac752bd55c
Victor Salome <vsalome41@gmail.com>
2024-05-14 13:05:41 -0300

    feat: implementação da função de sempre que trocar de pagina ela começar pelo topo

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,9 +1,15 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { ExploreWorldPage, HomePage } from "../pages";
 import { NavBar } from "../components";
 import { ExploreClansPages } from "../pages/exploreClansPage";
+import { useEffect } from "react";
 
 export const AppRouter: React.FC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [navigate]);
   return (
     <>
       <NavBar />
